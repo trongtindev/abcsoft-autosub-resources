bd994428c0b8f3394c6a27f71715ddf6681b0744
Tin Nguyen Trong <me@trongtin.dev>
2023-10-05 17:23:40 +0700

    bug fixes

--- resources/tesseract/tessdata.js
+++ resources/tesseract/tessdata.js
@@ -1,14 +1,14 @@
-const fs = require('fs');
-const isoConv = require('iso-language-converter');
+const fs = require("fs");
+const isoConv = require("iso-language-converter");
 
 const getFiles = (path) => {
   return fs
-    .readdirSync(`./resources/${path}`)
-    .filter((e) => e.endsWith('.traineddata'))
-    .filter((e) => !e.includes('_'))
+    .readdirSync(`./resources/tesseract/${path}`)
+    .filter((e) => e.endsWith(".traineddata"))
+    .filter((e) => !e.includes("_old"))
     .sort()
     .map((e) => {
-      const id = e.split('.')[0];
+      const id = e.split(".")[0];
       return {
         id: id,
         name: isoConv(id) ?? id,
@@ -17,10 +17,10 @@ const getFiles = (path) => {
 };
 
 fs.writeFileSync(
-  './resources/tessdata.json',
+  "./resources/tesseract/tessdata.json",
   JSON.stringify({
-    tessdata_best: getFiles('tessdata_best-4.1.0'),
-    tessdata_fast: getFiles('tessdata_fast-4.1.0'),
-    tessdata: getFiles('tessdata-4.1.0'),
+    tessdata_best: getFiles("tessdata_best-4.1.0"),
+    tessdata_fast: getFiles("tessdata_fast-4.1.0"),
+    tessdata: getFiles("tessdata-4.1.0"),
   })
 );
